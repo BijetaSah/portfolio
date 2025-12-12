d3ca5728d21fa360c2f7f54df80c093ba3ab46f5
Bijeta Sah <bijetasah30@gmail.com>
2025-12-12 18:31:31 +0530

    feat : create reusable section container  component to add padding and styles

---
+++ src/ui/SectionContainer.jsx
@@ -0,0 +1,12 @@
+function SectionContainer({ id, styles, children }) {
+  return (
+    <section
+      id={id}
+      className={` text-slate-300 md:py-20 px-10 py-10 md:px-15 ${styles}`}
+    >
+      {children}
+    </section>
+  );
+}
+
+export default SectionContainer;
