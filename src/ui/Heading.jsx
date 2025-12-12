d35aeeef4a59ffb82f182876e1f38f516c5cb1f3
Bijeta Sah <bijetasah30@gmail.com>
2025-12-12 18:30:06 +0530

    feat : create reusable heading component used across the app

---
+++ src/ui/Heading.jsx
@@ -0,0 +1,13 @@
+function Heading({ children, positionLeft = "left-4", width = "w-20" }) {
+  return (
+    <h2 className="text-2xl text-slate-100 font-semibold relative">
+      {children}
+      <span
+        className={`w-20 h-1 bg-linear-to-r from-blue-400 to-teal-500 rounded-sm absolute
+  ${positionLeft} ${width} -bottom-2`}
+      ></span>
+    </h2>
+  );
+}
+
+export default Heading;
