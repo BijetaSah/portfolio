4755c00b049026d9991e66a5f67c12cab8722d81
Bijeta Sah <bijetasah30@gmail.com>
2025-12-12 21:29:46 +0530

    refactor : fix padding issues

--- src/ui/SectionContainer.jsx
+++ src/ui/SectionContainer.jsx
@@ -2,7 +2,7 @@ function SectionContainer({ id, styles, children }) {
   return (
     <section
       id={id}
-      className={` text-slate-300 md:py-20 px-10 py-30 md:px-15 ${styles}`}
+      className={` text-slate-300 md:py-20 px-5 md:px-10 lg:px-25 py-30 ${styles}`}
     >
       {children}
     </section>
