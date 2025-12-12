ec81a4571b1657cc7dfcf5ed71f9a63a06b82068
Bijeta Sah <bijetasah30@gmail.com>
2025-12-12 22:12:47 +0530

    feat : add components to the layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+import About from "./components/about/About";
 import Navbar from "./components/header/Navbar";
 import Hero from "./components/hero/Hero";
+import Skills from "./components/skills/Skills";
 
 function App() {
   return (
     <main className="bg-slate-900">
       <Navbar />
       <Hero />
+      <About />
+      <Skills />
     </main>
   );
 }
