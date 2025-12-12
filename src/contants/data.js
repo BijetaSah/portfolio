a8ac614b0d1ec6bc57818d523a73ffbfa72e6155
Bijeta Sah <bijetasah30@gmail.com>
2025-12-12 16:16:40 +0530

    data : add data for socila links and nav links

--- src/contants/data.js
+++ src/contants/data.js
@@ -1,3 +1,6 @@
+import { TbBrandGithubFilled } from "react-icons/tb";
+import { FaLinkedin } from "react-icons/fa";
+
 export const navLinks = [
   {
     id: "about",
@@ -18,5 +21,22 @@ export const navLinks = [
   {
     id: "contact",
     title: "Contact",
+  },
+];
+
+export const socialLinks = [
+  {
+    name: "GitHub",
+    icon: TbBrandGithubFilled,
+    url: "https://github.com/BijetaSah",
+    background: "bg-gray-800",
+    shadowColor: "blue-500/25",
+  },
+  {
+    name: "LinkedIn",
+    icon: FaLinkedin,
+    url: "https://linkedin.com/in/bijeta-sah",
+    background: "bg-blue-800",
+    shadowColor: "blue-500/25",
   },
 ];
